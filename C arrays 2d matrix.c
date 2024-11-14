87e96a3448684b17be51609eb71b14f85ac647d5
kevchege <kevchege115@gmail.com>
2024-11-14 17:04:22 +0300

    Update C arrays 2d matrix.c

--- C arrays 2d matrix.c
+++ C arrays 2d matrix.c
@@ -3,16 +3,16 @@
 
 int main() {
 
-    int scores[2][2] = {
-        {65, 92},
-        {35, 70},
+    int scores[2][4] = {
+        {65,92,35,70},
+        {84,72,59,67}
 
     };
 
     // iii. Print the elements of the array using a nested for loop
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("%d ", scores[i][j]);
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("scores [%d][%d]=%d \n ",i,j, scores[i][j]);
         }
         printf("\n");
     }
